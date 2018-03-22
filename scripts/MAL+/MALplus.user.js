cdea129247e470505d79b47989378bcfb41cbf43
Simon Berger <siku2@outlook.de>
2018-03-22 11:55:30 +0100

    dis

--- scripts/MAL+/MALplus.user.js
+++ scripts/MAL+/MALplus.user.js
@@ -2,12 +2,17 @@
 // @name     MAL+
 // @icon     https://myanimelist.cdn-dena.com/img/sp/icon/apple-touch-icon-256.png
 // @include  /^http(?:s)?\:\/\/myanimelist\.net.*$/
-// @version  1.2
+// @version  1.3
 // @require  https://code.jquery.com/jquery-3.2.1.min.js
 // ==/UserScript==
 
-let animeSearchName = "9Anime";
-let animeSearchURL = "https://9anime.to/search?keyword=";
+const animeSearchName = "9Anime";
+const animeSearchURL = "https://9anime.to/search?keyword=";
+
+const adSearch = [
+  "Notice us",
+  "ads help us pay the bills"
+];
 
 
 function adRemove() {
@@ -21,7 +26,7 @@ function adRemove() {
       if (!nodeVal) {
         return false;
       }
-      return nodeVal.includes("Notice us");
+      return Boolean(adSearch.find(target => nodeVal.includes(target)));
     }).forEach(el => {
       for (let i = 0; i < 5; i++) {
         el = el.parentElement;
